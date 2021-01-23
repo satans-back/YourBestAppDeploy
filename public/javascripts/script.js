9363bb91c3740f5f207e1cdd0d925c9ca80c8807
przemyslawpomiechowski <p.pomiechowski@outlook.com>
2021-01-23 16:15:40 +0000

    azure storage + js

---
+++ public/javascripts/script.js
@@ -0,0 +1,26 @@
+function getProduct() {
+    var productDiv = document.createElement("div");
+    productDiv.className = "product"
+
+    var prodImg = document.createElement("img");
+    prodImg.src = "img/products/1.jpg";
+    prodImg.className = "product_img";
+
+    var productTitle = document.createElement("h3");
+    productTitle.className = "product_title";
+    productTitle.innerHTML = "Budynki mordoru, 23,99zl"
+
+    var productDesc = document.createElement("p");
+    productDesc.className = "product_desc";
+    productDesc.innerHTML = "This work is a phantasmagoric, urban world in which death and waste are omnipresent consisting of appropriated internet footage."
+
+    var spacer = document.createElement("div");
+    spacer.className = "spacer";
+    spacer.style.clear = "both";
+
+    document.getElementById("shop_view").appendChild(productDiv);
+    productDiv.appendChild(prodImg);
+    productDiv.appendChild(productTitle);
+    productDiv.appendChild(productDesc);
+    productDiv.appendChild(spacer);
+}
