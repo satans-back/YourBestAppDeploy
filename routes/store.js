dfa7324dea74d3b10a0af1bbe140e26e4288d51e
przemyslawpomiechowski <p.pomiechowski@outlook.com>
2021-01-27 22:21:40 +0000

    TABLE

--- routes/store.js
+++ routes/store.js
@@ -3,13 +3,24 @@ const table = 'prodcutsdb'
 var service = storage.createTableService('yourbestappstor', 'jJdxCY8ysmhX0QoKKIKMLAwkmg35IOPZBPRDq+RdZQFCgeceLfzPPioOMHNsKkeKX1dsis/4MGm16xRxbEm4kQ==');
 
 const init = async () => (
-new Promise((resolve, reject) => {
-    service.createTableIfNotExists(table, (error, result, response) => {
-        !error ? resolve() : reject()
+    new Promise((resolve, reject) => {
+        service.createTableIfNotExists(table, (error, result, response) => {
+            !error ? resolve() : reject()
+        })
     })
-})
 )
 
+const listProducts = async () => (
+    new Promise((resolve, reject) => {
+        tableSvc.queryEntities(table, query, null, function(error, result, response) {
+            if(!error) {
+              // query was successful
+            }
+        });
+    })
+  )
+
  module.exports = {
-   init
+   init,
+   listProducts
  }
